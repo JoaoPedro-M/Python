e137f900a9a59ff0327d2523ce0cfe6ad5bdb01d
João Pedro <62775155+JoaoPedro-M@users.noreply.github.com>
2020-06-25 20:52:07 -0300

    Update main.py
    parte de design basico

--- Cobrinha/main.py
+++ Cobrinha/main.py
@@ -3,10 +3,18 @@ import sys
 
 pygame.init()
 
-pygame.display.set_mode([300, 300])
+tela = pygame.display.set_mode([400, 400])
+pygame.display.set_caption('Jogo da Cobra')
+tempo = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
+
+    tela.fill((255, 255, 255))
+    pygame.display.update()
+    tempo.tick(30)
