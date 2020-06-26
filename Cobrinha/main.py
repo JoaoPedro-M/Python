c383d9325ce8f8e973f48dbe5cea8a73b956ebfd
João Pedro <62775155+JoaoPedro-M@users.noreply.github.com>
2020-06-25 22:30:43 -0300

    change in main.py
    perfeicao dos movimentos da cobra e criacao de um arquivo para testes

--- Cobrinha/main.py
+++ Cobrinha/main.py
@@ -1,10 +1,16 @@
 import pygame
 import sys
 
+
+def muda_lugar(x, y, dir, obj):
+    for c in obj:
+        c.lugares_mudar.append([x, y, dir])
+
 class Retangulos_Brancos(pygame.Rect):
     def __init__(self, pos, tam):
         super().__init__(pos, tam)
         self.direcao = 'cima'
+        self.lugares_mudar = []
 
     def mover(self):
         if self.direcao == 'cima':
@@ -16,11 +22,18 @@ class Retangulos_Brancos(pygame.Rect):
         elif self.direcao == 'esquerda':
             self.x -= 5
 
+    def verifica_direcao(self):
+        if len(self.lugares_mudar) > 0:
+            if self.x == self.lugares_mudar[0][0] and self.y == self.lugares_mudar[0][1]:
+                self.direcao = self.lugares_mudar[0][2]
+                self.lugares_mudar.pop(0)
+
 
 class Cobra:
     def __init__(self, tela):
         self.tela = tela
         self.partes = []
+        self.posx, self.posy = 0, 0
         for c in range(0, 5):
             self.partes.append(Retangulos_Brancos([200, 200+20*c], [10, 10]))
 
@@ -30,7 +43,21 @@ class Cobra:
 
     def andar(self):
         for c in self.partes:
+            c.verifica_direcao()
             c.mover()
+
+    def mudar_direcoes(self, event):
+        self.posx, self.posy = self.partes[0].x, self.partes[0].y
+        if event.key == pygame.K_DOWN:
+            muda_lugar(self.posx, self.posy, 'baixo', self.partes)
+        elif event.key == pygame.K_UP:
+            muda_lugar(self.posx, self.posy, 'cima', self.partes)
+        elif event.key == pygame.K_RIGHT:
+            muda_lugar(self.posx, self.posy, 'direita', self.partes)
+        elif event.key == pygame.K_LEFT:
+            muda_lugar(self.posx, self.posy, 'esquerda', self.partes)
+
+
 
 
 pygame.init()
@@ -46,6 +73,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            rec.mudar_direcoes(event)
     tela.fill((255, 255, 255))
 
     rec.mostrar()
