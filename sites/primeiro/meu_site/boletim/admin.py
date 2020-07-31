c8acc1c2f7278a8c2f7b8eb10afb990829be021d
João Pedro <62775155+JoaoPedro-M@users.noreply.github.com>
2020-07-31 14:40:41 -0300

    update

--- sites/primeiro/meu_site/boletim/admin.py
+++ sites/primeiro/meu_site/boletim/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
 admin.site.register(models.Materias)
+admin.site.register(models.Series)
+admin.site.register(models.Alunos)
+admin.site.register(models.MateriaAluno)
+admin.site.register(models.MateriasSeries)
--- sites/primeiro/meu_site/boletim/models.py
+++ sites/primeiro/meu_site/boletim/models.py
@@ -1,9 +1,41 @@
 from django.db import models
 
 
+class MateriasSeries(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+
+class MateriaAluno(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+
+class Series(models.Model):
+    materias = models.ForeignKey(to=MateriasSeries, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
 class Materias(models.Model):
+    series = models.ForeignKey(to=MateriasSeries, on_delete=models.CASCADE)
+    alunos = models.ForeignKey(to=MateriaAluno, on_delete=models.CASCADE)
     nome = models.CharField(max_length=50)
     nota = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Alunos(models.Model):
+    materias = models.ForeignKey(to=MateriaAluno, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
